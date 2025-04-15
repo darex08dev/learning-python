b607fcf2349326d624381bfac99ad96023feb2e8
darex08dev <suarezdavid0803@outlook.com>
2025-04-15 13:53:16 -0400

    Creación de archivo

---
+++ 1-numeros_primos.py
@@ -0,0 +1,20 @@
+def calc_num_primo(num):
+    
+    cont = 0
+    
+    for divisores in range(1, num+1):
+        
+       cont += 1 if (num % divisores) == 0
+            
+    return f'{num} no es un número primo' if cont != 2
+    
+    print(f'{num} es un número primo')
+
+def main():
+    
+    while True:
+        print('Numeros primos')
+        numero = int(input('Ingresa tu número: '))
+        
+if __name__ == '__main__':
+    main()
