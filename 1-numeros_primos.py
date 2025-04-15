56dfe9c4c66eb71f4eb226a12357c0eda4bd26af
darex08dev <suarezdavid0803@outlook.com>
2025-04-15 14:00:49 -0400

    Corrección de if ternario

--- 1-numeros_primos.py
+++ 1-numeros_primos.py
@@ -4,11 +4,9 @@ def calc_num_primo(num):
     
     for divisores in range(1, num+1):
         
-       cont += 1 if (num % divisores) == 0
+       cont += 1 if (num % divisores) == 0 else return None
             
-    return f'{num} no es un número primo' if cont != 2
-    
-    print(f'{num} es un número primo')
+    return f'{num} no es un número primo' if cont != 2 else print(f'{num} es un número primo')
 
 def main():
     
