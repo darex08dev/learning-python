bea65e8c2eabc7e0009e85bb960237b7c1cf5e18
darex08dev <suarezdavid0803@outlook.com>
2025-04-15 14:20:25 -0400

    Corrección de errores y bucle infinito

--- 1-numeros_primos.py
+++ 1-numeros_primos.py
@@ -3,16 +3,24 @@ def calc_num_primo(num):
     cont = 0
     
     for divisores in range(1, num+1):
-        
-       cont += 1 if (num % divisores) == 0 else return None
-            
-    return f'{num} no es un número primo' if cont != 2 else print(f'{num} es un número primo')
+       if (num % divisores) == 0:
+           cont += 1
+    
+    if cont == 2:
+        print(f'{num} es un número primo')
+    else:
+        print(f'{num} no es un número primo')
 
 def main():
     
     while True:
         print('Numeros primos')
         numero = int(input('Ingresa tu número: '))
+        calc_num_primo(numero)
+        opc = input('¿Desea continuar? (S/N): ').lower()
+        
+        if opc == 'n':
+            break
         
 if __name__ == '__main__':
     main()
